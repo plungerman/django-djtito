4aa2553cc56ed597c5c1814c622ae6133853f419
Bender <plungerman@gmail.com>
2015-10-19 17:25:07 -0500

    moved  home page redirect to lastpattern

--- djtito/newsletter/urls.py
+++ djtito/newsletter/urls.py
@@ -3,9 +3,9 @@ from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = patterns('djtito.newsletter.views',
     url(
-        r'^$', RedirectView.as_view(url="/bridge/")
+        r'^manager/$', 'manager', name='newsletter_manager'
     ),
     url(
-        r'^manager/$', 'manager', name='newsletter_manager'
+        r'^$', RedirectView.as_view(url="/bridge/")
     ),
 )
