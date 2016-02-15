45c5de5c46425d819f086f9d9aaa0f3fd257545f
Bender <plungerman@gmail.com>
2016-02-15 09:56:35 -0600

    migrated to django1.8

--- djtito/wsgi_default.py
+++ djtito/wsgi_default.py
@@ -1,9 +1,12 @@
-import os, sys
+import os
+import time
+import traceback
+import signal
+import sys
 
 sys.path.append('/usr/local/lib/python2.7/')
 sys.path.append('/usr/lib/python2.7/')
-sys.path.append('/data2/django_1.7/')
-#sys.path.append('/data2/django_current/')
+sys.path.append('/data2/django_1.8/')
 sys.path.append('/data2/django_projects/')
 sys.path.append('/data2/django_third/')
 
@@ -12,5 +15,14 @@ os.environ.setdefault("PYTHON_EGG_CACHE", "/var/cache/python/.python-eggs")
 os.environ.setdefault("TZ", "America/Chicago")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
 
+try:
+    application = get_wsgi_application()
+    #print 'WSGI without exception'
+except Exception:
+    #print 'handling WSGI exception'
+    # Error loading applications
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
