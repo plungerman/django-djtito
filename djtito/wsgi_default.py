df15b7b312c4165348cfd38d2fd3cc0dba251cf4
plungerman <plungerman@gmail.com>
2019-03-24 14:57:41 -0500

    updates from migration to ubuntu 18.04 hygiea server

--- djtito/wsgi_default.py
+++ djtito/wsgi_default.py
@@ -4,25 +4,26 @@ import traceback
 import signal
 import sys
 
-sys.path.append('/usr/local/lib/python2.7/')
-sys.path.append('/usr/lib/python2.7/')
-sys.path.append('/data2/django_1.8/')
-sys.path.append('/data2/django_projects/')
-sys.path.append('/data2/django_third/')
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djtito.settings")
-os.environ.setdefault("PYTHON_EGG_CACHE", "/var/cache/python/.python-eggs")
-os.environ.setdefault("TZ", "America/Chicago")
-
+# python
+sys.path.append('/data2/python_venv/2.7/djtito/lib/python2.7/')
+sys.path.append('/data2/python_venv/2.7/djtito/lib/python2.7/site-packages/')
+sys.path.append('/data2/python_venv/2.7/djtito/lib/django_projects/')
+sys.path.append('/data2/python_venv/2.7/djtito/lib/django-djtito/')
+# django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djtito.settings')
+os.environ.setdefault('PYTHON_EGG_CACHE', '')
+os.environ.setdefault('TZ', 'America/Chicago')
+# wsgi
 from django.core.wsgi import get_wsgi_application
 
+# NOTE: remove the try/except in production
+#application = get_wsgi_application()
 try:
     application = get_wsgi_application()
-    #print 'WSGI without exception'
 except Exception:
-    #print 'handling WSGI exception'
     # Error loading applications
     if 'mod_wsgi' in sys.modules:
         traceback.print_exc()
         os.kill(os.getpid(), signal.SIGINT)
         time.sleep(2.5)
+    exit(-1)
