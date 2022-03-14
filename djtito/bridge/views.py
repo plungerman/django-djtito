94c0edda6ce83d99e4b227d0de4a6c2a95158b82
plungerman <plungerman@gmail.com>
2022-03-14 15:54:25 -0500

    added rotating hero images to news screen

--- djtito/bridge/views.py
+++ djtito/bridge/views.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 
 import datetime
+import requests
 
 from django.conf import settings
 from django.shortcuts import render
 from djtito.core.models import LivewhaleEvents as Events
 from djtito.core.models import LivewhaleNews as News
 from djtito.utils import fetch_news
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
 def news(request):
@@ -24,5 +29,13 @@ def news(request):
     ).filter(status=1).filter(date_dt__lte=now).filter(
         is_archived__isnull=True,
     ).exclude(date_dt__lte=past)
+    # images
+    # https://www.carthage.edu/live/json/images/group/screens/tag/carthageviews
+    earl = '{0}/live/json/images/group/screens/tag/carthageviews'.format(
+        settings.LIVEWHALE_API_URL,
+    )
+    response = requests.get(earl)
+    jason = response.json()
+    photos = jason
 
-    return render(request, 'bridge/screens/news.html', {'news': news})
+    return render(request, 'bridge/screens/news.html', {'news': news, 'photos': photos})
