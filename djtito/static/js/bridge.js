294ac4ccf952cdfc1655f9fb9608475c32541b2c
plungerman <plungerman@gmail.com>
2022-03-28 09:40:45 -0500

    minor updates for communications folks

--- djtito/static/js/bridge.js
+++ djtito/static/js/bridge.js
@@ -10,11 +10,11 @@ $(function() {
       current.removeClass('active');
     });
     // repeat after 5 seconds
-    setTimeout(displayLoop, 10000);
+    setTimeout(displayLoop, 15000);
   }
 
   // Display the first sidebar item when the page loads, then call the display loop method
-  $('.lw_widget_news li:first-child').addClass('active').fadeIn('slow').delay(10000, 'displayQueue').queue('displayQueue', function(){
+  $('.lw_widget_news li:first-child').addClass('active').fadeIn('slow').delay(15000, 'displayQueue').queue('displayQueue', function(){
     displayLoop();
   }).dequeue('displayQueue');
 });
