06ee297f6ef6895791943b4c348973627c39c409
plungerman <plungerman@gmail.com>
2023-03-14 10:13:39 -0500

    fix requests.get() call to livehwhale api to avoid timeout errors

--- djtito/bridge/views.py
+++ djtito/bridge/views.py
@@ -16,32 +16,51 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 def events(request):
     """Events for digital signage."""
+    events = None
+    photos = None
     earl = '{0}/live/json/events/group/screens/max/24/'.format(
         settings.LIVEWHALE_API_URL,
     )
-    response = requests.get(earl)
-    events = response.json()
-    # images
-    earl = '{0}/live/json/images/group/screens/tag/carthageviews/max/8/'.format(
-        settings.LIVEWHALE_API_URL,
-    )
-    response = requests.get(earl)
-    photos = response.json()
+    try:
+        response = requests.get(earl,  timeout=10)
+    except Exception:
+        response = None
+    if response:
+        events = response.json()
+        # images
+        earl = '{0}/live/json/images/group/screens/tag/carthageviews/max/8/'.format(
+            settings.LIVEWHALE_API_URL,
+        )
+        try:
+            response = requests.get(earl,  timeout=10)
+        except Exception:
+            resposne = None
+        if response:
+            photos = response.json()
     return render(request, 'bridge/screens/events.html', {'events': events, 'photos': photos})
 
 
 def news(request):
     """News for digital signage."""
+    news = None
+    photos = None
     earl = '{0}/live/json/news/group/screens/max/24/'.format(
         settings.LIVEWHALE_API_URL,
     )
-    response = requests.get(earl)
-    news = response.json()
-    # images
-    earl = '{0}/live/json/images/group/screens/tag/carthageviews/max/8/'.format(
-        settings.LIVEWHALE_API_URL,
-    )
-    response = requests.get(earl)
-    photos = response.json()
-
+    try:
+        response = requests.get(earl,  timeout=10)
+    except Exception:
+        response = None
+    if response:
+        news = response.json()
+        # images
+        earl = '{0}/live/json/images/group/screens/tag/carthageviews/max/8/'.format(
+            settings.LIVEWHALE_API_URL,
+        )
+        try:
+            response = requests.get(earl,  timeout=10)
+        except Exception:
+            response = None
+        if response:
+            photos = response.json()
     return render(request, 'bridge/screens/news.html', {'news': news, 'photos': photos})
