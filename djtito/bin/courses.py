11f4eafb4c33b8f8f91c18e052570a2b99c299ba
plungerman <plungerman@gmail.com>
2025-08-04 09:45:19 -0500

    updated procedure steps; updated course object creation based on new values for column numbers in the data row

--- djtito/bin/courses.py
+++ djtito/bin/courses.py
@@ -25,16 +25,6 @@ parser.add_argument(
     dest='phile',
 )
 
-# CSV file format
-[
-    'Buddhism (REL) (DIV)(CL)(WEL)',
-    'REL',
-    'REL 3130',
-    'Religion',
-    "4cr  An intensive look at the world's oldest missionary religion, from its origin in the Ganges basin in 500 BCE to its contemporary manifestations. The course's primary emphasis will be on the historical development of the tradition, and the ways that its message has been transformed through the influence of different cultures, including the United States. An important part of this will be closely examining the Buddhist way of life throughout the centuries, and the ways in which this ancient message is still relevant in the modern world.",
-    '4', '4', '4 Semester Credits',
-]
-
 
 def main():
     """
@@ -44,9 +34,9 @@ def main():
 
     1) execute destroy.py to dump the catalog.
 
-    2) import the UG* courses:
+    2) import the undergraduate courses:
 
-    python course.py --file=undergraduate.csv
+    python courses.py --file=undergraduate.csv
 
     3) execute the following SQL incantation:
 
@@ -60,7 +50,11 @@ def main():
     update course_catalog set disc="MAT" where dept="ATH" and disc="ATH";
     update course_catalog set disc="BUS" where dept="MGT" and disc="";
 
-    4) generate the PDF with the prince command:
+    4) import the graduate courses:
+
+    python courses.py --file=rgraduate.csv
+
+    5) generate the PDF with the prince command:
 
     ssh eros.carthage.edu
     cd /d2/www/vhosts/carthage.edu/subdomains/eros/httpdocs/catalog/
@@ -68,7 +62,7 @@ def main():
     /usr/local/bin/prince \
     https://app.carthage.edu/djtito/catalog/print/ -o catalog.pdf
 
-    9) optionally, set up a cronjob that runs every five minutes:
+    6) optionally, set up a cronjob that runs every five minutes:
 
     */5 * * * * /usr/local/bin/prince https://app.carthage.edu/djtito/catalog/print/
     --output=/d2/www/vhosts/carthage.edu/subdomains/eros/httpdocs/catalog/catalog.pdf >> /dev/null 2>&1
@@ -77,14 +71,15 @@ def main():
         csv_reader = csv.reader(csv_file, delimiter='|')
         next(csv_reader, None)  # skip the headers
         for row in csv_reader:
+
             course = Course.objects.using('workday').create(
                 title=row[0],
-                dept=row[1],
-                disc=row[1],
-                crs_no=row[2],
-                abstr=row[4],
+                dept=row[2],
+                disc=row[2],
+                crs_no=row[3],
+                abstr=row[5],
             )
-            print(course)
+            print(course.crs_no, course.title)
 
 
 if __name__ == '__main__':
